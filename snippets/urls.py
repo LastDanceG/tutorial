494c8ddf36fa7b56ec336628d43a61392aa57cbf
LastDanceG <L5623064@163.com>
2021-02-07 18:01:02 +0800

    修改url中冗余的代码

--- snippets/urls.py
+++ snippets/urls.py
@@ -64,10 +64,6 @@ user_detail = UserViewSet.as_view({
 #
 # ])
 
-# urlpatterns += [
-#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
-
 
 # 使用Router重写路由定义
 router = DefaultRouter()
